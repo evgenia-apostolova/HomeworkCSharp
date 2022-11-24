2c62cf1f03dc586e9ab6b5e3432f6379c75a0775
Evgenia <evgenia.apostolova@gmail.com>
2022-11-24 14:55:12 +0300

    code corrected

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -5,21 +5,27 @@ a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */
 
-int a = new int();
+int max = 0;
+int min = 0;
+int number1 = new int();
+int number2 = new int();
+
 Console.WriteLine("Enter number1");
-a = Convert.ToInt32(Console.ReadLine());
+number1 = Convert.ToInt32(Console.ReadLine());
 
-int b = new int();
 Console.WriteLine("Enter number2");
-b = Convert.ToInt32(Console.ReadLine());
+number2 = Convert.ToInt32(Console.ReadLine());
 
-int max = new int();
-
-if (a > b)
-max = a;
-
+if (number1 > number2)
+{
+    max = number1;
+    min = number2;
+}
 else
-max = b;
+{
+    max = number2;
+    min = number1;
+}
 
-Console.Write("max = ");
-Console.WriteLine(max);
+Console.WriteLine("max = " + max);
+Console.WriteLine("min = " + min);
