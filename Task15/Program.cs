fcf7912c13fc3dfcd1f48fb9c5523b55df1aec2e
Evgenia <evgenia.apostolova@gmail.com>
2022-11-28 19:40:00 +0300

    Task15 added

---
+++ Task15/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую
+день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+
+Console.WriteLine("Enter a daynumber of a weekday");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+if (dayNumber > 5)
+    
+Console.WriteLine("This day is a dayoff");
+
+if (dayNumber <= 5)
+
+Console.WriteLine("This day is a workday");
+
+
