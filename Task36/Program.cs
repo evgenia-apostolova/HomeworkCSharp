126d2e7700812274d8e921908d0cd066797781a7
Evgenia <evgenia.apostolova@gmail.com>
2022-12-01 16:49:35 +0300

    Task36 added

---
+++ Task36/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Задайте одномерный массив, заполненный случайными числами.
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+int[] generateRandomArray(int length, int start, int end)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(start, end + 1);
+    }
+    return array;
+}
+
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int getSumOfOddIndexElements(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+            sum = sum + array[i];
+    }
+    return sum;
+}
+
+int[] generatedArray = generateRandomArray(8, -10, 10);
+printArray(generatedArray);
+int sum = getSumOfOddIndexElements(generatedArray);
+Console.WriteLine($"Sum of array elements with odd indices is {sum}.");
