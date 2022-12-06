79aa5f3c30ad6cf8d7bf662871991b4d255c9ee9
Evgenia <evgenia.apostolova@gmail.com>
2022-12-07 02:23:40 +0300

    Task54added

---
+++ Task54/Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу, которая
+упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+int[,] generateArray(int lengthRow, int lengthCol)
+{
+    int[,] array = new int[lengthRow, lengthCol];
+    for (int i = 0; i < lengthRow; i++)
+    {
+        for (int j = 0; j < lengthCol; j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+void printArray(int[,] array, string Name = "")
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] sortArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k];
+                    array[i, k] = array[i, k + 1];
+                    array[i, k + 1] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+int[,] generatedArray = generateArray(4, 5);
+Console.WriteLine("Initial array:");
+printArray(generatedArray);
+Console.WriteLine();
+int[,] sortedArray = sortArray(generatedArray);
+Console.WriteLine("Sorted array:");
+printArray(generatedArray);
+Console.WriteLine();
